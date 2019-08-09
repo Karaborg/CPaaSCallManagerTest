eb84455506dbd525ae810b8ec35b06bd97d96a7f
Furkan Cırıt <34005617+Karaborg@users.noreply.github.com>
2019-08-09 10:30:01 +0300

    Update CPaaSCallManagerTest.java

--- CPaaSCallManagerTest.java
+++ CPaaSCallManagerTest.java
@@ -5,6 +5,8 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.rbbn.cpaas.mobile.demo_java.ui.main.EnterenceActivity;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +17,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -28,20 +31,19 @@ import static org.hamcrest.Matchers.is;
 @RunWith(AndroidJUnit4.class)
 public class CPaaSCallManagerTest {
 
+    private Boolean isLoginSucceed = false;
+
     @Rule
-    public ActivityTestRule<EnterenceActivity> loginFragmentActivityTestRule = new ActivityTestRule<>(EnterenceActivity.class);
+    public ActivityTestRule<EnterenceActivity> mActivity = new ActivityTestRule<>(EnterenceActivity.class);
+
+    @Before
+    public void setup(){
+
+    }
 
     @Test
-    public void testLoginCPaaSMobileSDK() throws InterruptedException {
-        sleep(2000);
-        // Click on the Spinner
-        onView(withId(R.id.platformList_LoginActivity_Spinner)).perform(click());
-        // Click on the first item from the list, which is a marker string: "Select your country"
-        onData(allOf(is(instanceOf(String.class)))).atPosition(0).perform(click());
-        onView(withId(R.id.get_new_token_button)).perform(click());
-        sleep(5000);
-        onView(withId(R.id.login_form)).perform(swipeUp());
-        onView(withId(R.id.login_button)).perform(click());
+    public void testIsLoginSucceeded() throws InterruptedException {
+        loginMethod();
         sleep(2000);
         onView(withId(R.id.radio_video)).check(matches(isDisplayed()));
     }
@@ -65,20 +67,27 @@ public class CPaaSCallManagerTest {
         onView(withId(R.id.radio_video)).check(matches(isDisplayed()));
     }
 
-    public void LogOut() throws InterruptedException {
-        sleep(2000);
+    @After
+    public void tearDown() {
+        if (isLoginSucceed){
+            logoutMethod();
+            isLoginSucceed = false;
+        }
+    }
+
+    public void loginMethod() throws InterruptedException {
+        sleep(1000);
         onView(withId(R.id.platformList_LoginActivity_Spinner)).perform(click());
         onData(allOf(is(instanceOf(String.class)))).atPosition(0).perform(click());
         onView(withId(R.id.get_new_token_button)).perform(click());
-        sleep(5000);
+        sleep(3000);
         onView(withId(R.id.login_form)).perform(swipeUp());
         onView(withId(R.id.login_button)).perform(click());
         sleep(2000);
-        onView(withId(R.id.radio_video)).check(matches(isDisplayed()));
-        //onView(withId(R.id.button)).perform(click());
-
+        isLoginSucceed = true;
     }
 
-
-
+    public void logoutMethod() {
+        onView(withId(R.id.radio_singlemline)).perform(swipeRight());
+    }
 }
